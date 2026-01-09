26ff166efd77b92f535735718b60a85f2fe35e1f
ARSHAL-16 <arshal.code@gmail.com>
2026-01-09 21:46:25 +0530

    Array questions

--- src/B_Arrays/arrays.java
+++ src/B_Arrays/arrays.java
@@ -232,18 +232,179 @@ public class arrays {
         for (int i = 1; i < arr.length; i++) {
             prefix[i] = prefix[i - 1] + arr[i];
         }
-        int maxLength=0;
-        for(int start=0;start<arr.length;start++){
-            for(int end=start;end<arr.length;end++){
-                int sum= start==0?prefix[end]:prefix[end]-prefix[start-1];
-                if(sum==k){
-                    maxLength= Math.max(maxLength,end-start+1);
+        int maxLength = 0;
+        for (int start = 0; start < arr.length; start++) {
+            for (int end = start; end < arr.length; end++) {
+                int sum = start == 0 ? prefix[end] : prefix[end] - prefix[start - 1];
+                if (sum == k) {
+                    maxLength = Math.max(maxLength, end - start + 1);
                 }
             }
         }
         return maxLength;
     }
 
-    ///////////////////////////////// MEDIUM ////////////////////////////////////
+    /// ////////////////////////////// MEDIUM ////////////////////////////////////
+
+    //TWO SUM
+    public int[] twoSum(int[] arr, int key) {
+        if (arr == null || arr.length < 2) {
+            return new int[]{-1, -1};
+        }
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            int rem = key - arr[i];
+
+            if (map.containsKey(rem)) {
+                return new int[]{map.get(rem), i};
+            }
+
+            map.put(arr[i], i);
+        }
+
+        return new int[]{-1, -1};
+    }
+
+    //MAJORITY ELEMENT
+    public static int majorityElement(int arr[]) {
+        int majorCount = arr.length / 2;
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        for (int num : arr) {
+            hm.put(num, hm.getOrDefault(num, 0) + 1);
+            if (hm.get(num) > majorCount) return num;
+        }
+        return Integer.MIN_VALUE;
+    }
+
+    //MAXIMUM SUBARRAY SUM
+    public static int maxSum(int arr[]) {
+        if (arr == null || arr.length == 0) return Integer.MIN_VALUE;
+        int maxSum = Integer.MIN_VALUE;
+        int currSum = 0;
+        //KADANE ALGO
+        for (int num : arr) {
+            currSum += num;
+            maxSum = Math.max(currSum, maxSum);
+            if (currSum < 0) {
+                currSum = 0;
+            }
+
+        }
+
+        return maxSum;
+    }
+
+    //STOCK BUY AND SELL
+    public static int buyAndSell(int arr[]) {
+        if (arr == null || arr.length < 2) return 0;
+        int profit = 0;
+        int lowestPrice = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            profit = Math.max(profit, arr[i] - lowestPrice);
+            lowestPrice = Math.min(lowestPrice, arr[i]);
+        }
+        return profit;
+
+    }
+
+    //FIND NEXT LEXICOGRAPHICALLY GREATER PERMUTATION
+    public static int[] greaterPermutation(int arr[]) {
+        if (arr == null || arr.length == 1) {
+            return arr;
+        }
+        //FINDING BREAKING-POINT
+        int breakingPoint = -1;
+        for (int i = arr.length - 2; i >= 0; i--) {
+            if (arr[i] < arr[i + 1]) {
+                breakingPoint = i;
+                break;
+            }
+        }
+        // THE ENTIRE ARRAY TO THE RIGHT OF BREAKING POINT WILL BE ARRANGED IN DESCENDING ORDER
+        //FINDING THE SMALLEST ELEMENT GREATER THAN ELE ON BREAKING-POINT, FORM THE RIGHT PART OF ARR
+        // AND SWAPPING IT WITH ARR[BREAKING POINT]
+        // LEFT PART | BREAKING POINT | RIGHT PART
+
+        if (breakingPoint != -1) {
+            for (int i = arr.length - 1; i > breakingPoint; i--) {
+                if (arr[i] > arr[breakingPoint]) {
+                    //swap
+                    int temp = arr[i];
+                    arr[i] = arr[breakingPoint];
+                    arr[breakingPoint] = temp;
+                    break;
+                    //EVEN AFTER DOING THIS RIGHT PART WILL BE SORTED IN DES... ORDER
+                }
+            }
+            //NOW REVERSE THE RIGHT PART, MAKING IT IN ASC ORDER
+            int start = breakingPoint + 1, end = arr.length - 1;
+            while (start < end) {
+                int temp = arr[start];
+                arr[start] = arr[end];
+                arr[end] = temp;
+                start++;
+                end--;
+            }
+        } else {
+            int start = 0, end = arr.length - 1;
+            while (start < end) {
+                int temp = arr[start];
+                arr[start] = arr[end];
+                arr[end] = temp;
+                start++;
+                end--;
+            }
+        }
+
+
+        return arr;
+    }
+
+    //LEADERS IN AN ARRAY
+    public static ArrayList<Integer> leadersInArray(int arr[]) {
+        if (arr == null || arr.length == 0) return new ArrayList<>();
+
+        ArrayList<Integer> al = new ArrayList<>();
+        al.add(arr[arr.length - 1]);
+        int currMax = arr[arr.length - 1];
+        for (int i = arr.length - 2; i >= 0; i--) {
+            if (arr[i] >= currMax) {
+                al.add(arr[i]);
+                currMax = arr[i];
+            }
+        }
+        return al;
+
+    }
+
+    //LONGEST CONSECUTIVE SEQUENCE
+    public static int longestSequence(int nums[]) {
+        if (nums == null) return 0;
+        if (nums.length == 1 || nums.length == 0) return nums.length;
+
+        HashSet<Integer> hs = new HashSet<>();
+
+        for (int num : nums) {
+            hs.add(num);
+        }
+        int longestSeq = 0;
+        for (int num : hs) {
+            if (!hs.contains(num - 1)) {
+                int currNum = num;
+                int count = 1;
+                while (hs.contains(currNum + 1)) {
+                    count++;
+                    currNum++;
+                }
+                longestSeq = Math.max(longestSeq, count);
+
+            }
+        }
+        return longestSeq;
+
+    }
+    ///////////////////////////////// HARD ////////////////////////////////////
 
 }
