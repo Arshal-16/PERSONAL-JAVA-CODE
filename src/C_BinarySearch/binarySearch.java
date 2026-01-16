c627eac1a3503d950cd8b4e635cf1f940c367e50
ARSHAL-16 <arshal.code@gmail.com>
2026-01-16 13:15:22 +0530

    Binary Search questions

--- src/C_BinarySearch/binarySearch.java
+++ src/C_BinarySearch/binarySearch.java
@@ -259,6 +259,29 @@ public class binarySearch {
         return -1;
     }
 
+    //FIND PEAK ELEMENT
+    public static int peakElement(int arr[]){
+        if(arr.length==1 || arr[0]>arr[1]) return 0;
+
+        if(arr[arr.length-1]>arr[arr.length-2]) return arr.length-1;
+
+        int start =1,end=arr.length-2;
+
+        while(start<=end){
+            int mid=(start+end)/2;
+            if(arr[mid]>=arr[mid-1]&&arr[mid]>=arr[mid+1]){
+                return mid;
+            }else if(arr[mid]>arr[mid-1]){
+                start=mid+1;
+            }else if(arr[mid]>arr[mid+1]){
+                end=mid-1;
+            }else{
+                start=mid+1;
+            }
+        }
+        return -1;
+    }
+
     /////////////// BS ON ANSWERS ////////////////////
 
 
