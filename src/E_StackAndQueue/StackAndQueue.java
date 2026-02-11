ff2ffc53284f0ed5f874b9ada85bc5895256952b
ARSHAL-16 <arshal.code@gmail.com>
2026-02-11 21:12:34 +0530

    Stack and Queue Questions

--- src/E_StackAndQueue/StackAndQueue.java
+++ src/E_StackAndQueue/StackAndQueue.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 public class StackAndQueue {
 
-    ////////////////// LEARNING /////////////////
+    /// /////////////// LEARNING /////////////////
 
     //IMPLEMENT STACK USING ARRAYS
     class StackUsingArray {
@@ -132,21 +132,24 @@ public class StackAndQueue {
     }
 
     //IMPLEMENT STACK USING LINKED LIST
-    class StackUsingLL{
+    class StackUsingLL {
         LinkedList<Integer> ll = new LinkedList<>();
 
-        boolean isEmpty(){
+        boolean isEmpty() {
             return ll.isEmpty();
         }
-        void push(int data){
+
+        void push(int data) {
             ll.addFirst(data);
         }
-        int pop(){
-            if(isEmpty()) throw new NoSuchElementException();
+
+        int pop() {
+            if (isEmpty()) throw new NoSuchElementException();
             return ll.removeFirst();
         }
-        int peek(){
-            if(isEmpty()) throw new NoSuchElementException();
+
+        int peek() {
+            if (isEmpty()) throw new NoSuchElementException();
             return ll.getFirst();
         }
 
@@ -154,41 +157,45 @@ public class StackAndQueue {
     }
 
     //IMPLEMENT QUEUE USING LINKED LIST
-    class QueueUsingLL{
-    LinkedList<Integer> ll = new LinkedList<>();
-    boolean isEmpty(){
-        return ll.isEmpty();
-    }
-    void add(int data){
-        ll.addLast(data);
-    }
-    int remove(){
-        if (isEmpty()) throw new NoSuchElementException();
-        return ll.removeFirst();
-    }
-    int peek(){
-        if(isEmpty()) throw new NoSuchElementException();
-        return ll.getFirst();
-    }
+    class QueueUsingLL {
+        LinkedList<Integer> ll = new LinkedList<>();
+
+        boolean isEmpty() {
+            return ll.isEmpty();
+        }
+
+        void add(int data) {
+            ll.addLast(data);
+        }
+
+        int remove() {
+            if (isEmpty()) throw new NoSuchElementException();
+            return ll.removeFirst();
+        }
+
+        int peek() {
+            if (isEmpty()) throw new NoSuchElementException();
+            return ll.getFirst();
+        }
 
     }
 
     //CHECK FOR BALANCED PARENTHESES
-    boolean checkForBalancedParenthesis(String str){
-        if(str==null || str.length()==0) return true;
+    boolean checkForBalancedParenthesis(String str) {
+        if (str == null || str.length() == 0) return true;
         Stack<Character> s = new Stack<>();
-        for(int itr = 0;itr<str.length();itr++){
-            char ch= str.charAt(itr);
-            if(ch=='(' || ch=='{' || ch=='['){
+        for (int itr = 0; itr < str.length(); itr++) {
+            char ch = str.charAt(itr);
+            if (ch == '(' || ch == '{' || ch == '[') {
                 s.push(ch);
-            }else if(ch==')'){
-                if(s.isEmpty() || s.peek()!='(') return false;
+            } else if (ch == ')') {
+                if (s.isEmpty() || s.peek() != '(') return false;
                 s.pop();
-            }else if(ch=='}'){
-                if(s.isEmpty() || s.peek()!='{') return false;
+            } else if (ch == '}') {
+                if (s.isEmpty() || s.peek() != '{') return false;
                 s.pop();
-            }else if(ch==']'){
-                if(s.isEmpty() || s.peek()!='[') return false;
+            } else if (ch == ']') {
+                if (s.isEmpty() || s.peek() != '[') return false;
                 s.pop();
             }
         }
@@ -196,48 +203,184 @@ public class StackAndQueue {
         return s.isEmpty();
 
         // why used stack?
-        // Because parentheses must be closed in reverse order of opening (LIFO), which is exactly what a stack provides.
+        // Because parentheses must be closed in reverseStr order of opening (LIFO), which is exactly what a stack provides.
     }
 
     //IMPLEMENT MIN STACK
-    class MinStack{
+    class MinStack {
         Stack<Integer> sNum = new Stack<>();
         Stack<Integer> sMin = new Stack<>();
 
-        boolean isEmpty(){
+        boolean isEmpty() {
             return sNum.isEmpty();
         }
 
-        void push(int data){
-            if(sNum.isEmpty()){
+        void push(int data) {
+            if (sNum.isEmpty()) {
                 sNum.push(data);
                 sMin.push(data);
                 return;
             }
             sNum.push(data);
 
-            if( Math.min(sMin.peek(),data) == data) sMin.push(data);
+            if (Math.min(sMin.peek(), data) == data) sMin.push(data);
             return;
         }
-        int pop(){
-            if(sNum.isEmpty()) throw new NoSuchElementException();
+
+        int pop() {
+            if (sNum.isEmpty()) throw new NoSuchElementException();
             int result = sNum.pop();
-            if(result == sMin.peek()) sMin.pop();
+            if (result == sMin.peek()) sMin.pop();
             return result;
         }
 
-        int peek(){
-            if(sNum.isEmpty()) throw new NoSuchElementException();
+        int peek() {
+            if (sNum.isEmpty()) throw new NoSuchElementException();
             return sNum.peek();
         }
-        int getMin(){
-            if(sMin.isEmpty()) throw new NoSuchElementException();
+
+        int getMin() {
+            if (sMin.isEmpty()) throw new NoSuchElementException();
             return sMin.peek();
         }
 
     }
 
+    /// /////////////// PREFIX, INFIX, POSTFIX /////////////////
 
+    //INFIX TO POSTFIX
+    String infixToPostfix(String infix) {
+        if (infix == null || infix.length() == 0) return infix;
+
+        Stack<Character> operator = new Stack<>();
+        StringBuilder ans = new StringBuilder();
+
+        for (int i = 0; i < infix.length(); i++) {
+            char ch = infix.charAt(i);
+
+            // If operand
+            if (Character.isLetterOrDigit(ch)) {
+                ans.append(ch);
+            }
+
+            // If '('
+            else if (ch == '(') {
+                operator.push(ch);
+            }
+
+            // If ')'
+            else if (ch == ')') {
+                while (!operator.isEmpty() && operator.peek() != '(') {
+                    ans.append(operator.pop());
+                }
+                if (!operator.isEmpty()) {
+                    operator.pop();
+                }
+            }
+
+            // If operator
+            else {
+                while (!operator.isEmpty() && operator.peek() != '(' &&
+                        (priority(ch) < priority(operator.peek()) ||
+                                (priority(ch) == priority(operator.peek()) && ch != '^'))) { //^ is right associative
+
+                    ans.append(operator.pop());
+                }
+                operator.push(ch);
+            }
+        }
+
+        while (!operator.isEmpty()) {
+            ans.append(operator.pop());
+        }
+
+        return ans.toString();
+    }
+
+    int priority(char optr) {
+        if (optr == '^') return 3;
+        else if (optr == '*' || optr == '/') return 2;
+        else if (optr == '+' || optr == '-') return 1;
+        return -1;
+    }
+
+    //INFIX TO PREFIX
+    String infixToPrefix(String infix) {
+        if (infix == null || infix.length() == 0) return infix;
+        //reverseStr the infix
+        infix = reverseStr(infix);
+        //swap the ( and ) brackets
+        infix=swapBrackets(infix);
+        //apply infix to postfix logic but handle ^ right associativity
+        Stack<Character> operator = new Stack<>();
+        StringBuilder ans = new StringBuilder();
+
+        for (int i = 0; i < infix.length(); i++) {
+            char ch = infix.charAt(i);
+
+            // If operand
+            if (Character.isLetterOrDigit(ch)) {
+                ans.append(ch);
+            }
+
+            // If '('
+            else if (ch == '(') {
+                operator.push(ch);
+            }
+
+            // If ')'
+            else if (ch == ')') {
+                while (!operator.isEmpty() && operator.peek() != '(') {
+                    ans.append(operator.pop());
+                }
+                if (!operator.isEmpty()) {
+                    operator.pop();
+                }
+            }
+
+            // If operator
+            else {
+                while (!operator.isEmpty() && operator.peek() != '(' &&
+                        (priority(ch) < priority(operator.peek()) ||
+                                (priority(ch) == priority(operator.peek()) && ch == '^'))) { //^ is right associative
+
+                    ans.append(operator.pop());
+                }
+                operator.push(ch);
+            }
+        }
+
+        while (!operator.isEmpty()) {
+            ans.append(operator.pop());
+        }
+
+        return reverseString(ans.toString());
+    }
+
+    String reverseString(String str) {
+        StringBuilder sb = new StringBuilder(str);
+        int start = 0, end = sb.length() - 1;
+        while (start < end) {
+            char temp = sb.charAt(end);
+            sb.setCharAt(end, sb.charAt(start));
+            sb.setCharAt(start, temp);
+            start++;
+            end--;
+        }
+        return sb.toString();
+    }
+
+    String swapBrackets(String str) {
+        StringBuilder sb = new StringBuilder(str);
+        for (int i = 0; i < sb.length(); i++) {
+            if (sb.charAt(i) == '(') {
+                sb.setCharAt(i, ')');
+            } else if (sb.charAt(i) == ')') {
+                sb.setCharAt(i, '(');
+            }
+        }
+        return sb.toString();
+    }
 
 }
 
